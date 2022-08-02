d48d5759b2b2ec529b81c8592031f4e80ee9e62e
Lubaiba Zakaria <v-lzakaria@traveldoo.com>
2022-08-02 15:47:25 +0530

    Initial Commit

---
+++ hotel-search-api/src/main/java/com/hotel/api/search/HotelSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.api.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSearchApiApplication.class, args);
+	}
+
+}
