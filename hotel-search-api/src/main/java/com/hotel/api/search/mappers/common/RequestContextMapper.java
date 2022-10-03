2b3428cb182256b92add05dca80127c8dc29f514
Keerthika Govindaraj <a-10420@ibsplc.com>
2022-10-03 13:25:49 +0530

    [TECH-475] Vault Config setup

--- hotel-search-api/src/main/java/com/hotel/api/search/mappers/common/RequestContextMapper.java
+++ hotel-search-api/src/main/java/com/hotel/api/search/mappers/common/RequestContextMapper.java
@@ -1,5 +1,6 @@
 package com.hotel.api.search.mappers.common;
 
+import com.hotel.api.search.model.SupplierCredential;
 import com.hotel.service.common.Context;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,23 +10,19 @@ import static com.hotel.service.util.ProtoBufUtil.safeSetProtoField;
 @Component
 public class RequestContextMapper {
     private final String url;
-    private final String requestorId;
-    private final String credential;
 
+    private final SupplierCredential supplierCredential;
 
-    public RequestContextMapper(@Value("${supplier.endPoint.url}") String url,
-                                @Value("${supplier.endPoint.requestorId}") String requestorId,
-                                @Value("${supplier.endPoint.credential}") String credential) {
+    public RequestContextMapper(@Value("${supplier.endPoint.url}") String url, SupplierCredential supplierCredential) {
         this.url = url;
-        this.requestorId = requestorId;
-        this.credential = credential;
+        this.supplierCredential = supplierCredential;
     }
 
     public Context map(){
         Context.Builder contextBuilder = Context.newBuilder();
         safeSetProtoField(contextBuilder::setSupplierUrl, url);
-        safeSetProtoField(contextBuilder::setSupplierRequestorId, requestorId);
-        safeSetProtoField(contextBuilder::setSupplierCredential, credential);
+        safeSetProtoField(contextBuilder::setSupplierRequestorId, supplierCredential.getRequestorId());
+        safeSetProtoField(contextBuilder::setSupplierCredential, supplierCredential.getPassword());
 
         return contextBuilder.build();
 
