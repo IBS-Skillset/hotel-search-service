8784c7bbd2c6fd7f699f6d4c112469ab7c2c08d8
Lubaiba Zakaria <v-lzakaria@traveldoo.com>
2022-10-20 13:19:38 +0530

    [TECH-479]Auth config

--- hotel-search-api/src/main/java/com/hotel/api/search/HotelSearchApiApplication.java
+++ hotel-search-api/src/main/java/com/hotel/api/search/HotelSearchApiApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableEurekaClient
 @EnableDiscoveryClient
@@ -16,16 +13,5 @@ public class HotelSearchApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelSearchApiApplication.class, args);
 	}
-
-    // Todo remove after APIgateway
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-            }
-        };
-    }
 
 }
---
+++ hotel-search-api/src/main/java/com/hotel/api/search/config/ResourceServerConfig.java
@@ -0,0 +1,21 @@
+package com.hotel.api.search.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class ResourceServerConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.oauth2ResourceServer(
+                j -> j.jwt().jwkSetUri("http://localhost:9000/auth-server/oauth2/jwks")
+        ).authorizeRequests()
+                .anyRequest().authenticated()
+                .and().build();
+    }
+
+}
