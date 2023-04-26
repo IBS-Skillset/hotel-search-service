712c9d8014fecc8e36ce98193144839eb1143a6e
bkalarikkal <v-bkalarikkal@Traveldoo.com>
2023-04-26 10:42:36 +0530

    [AWS-DEPLOYMENT] - corrections for deployment

--- hotel-search-api/src/main/java/com/hotel/api/search/config/ResourceServerConfig.java
+++ hotel-search-api/src/main/java/com/hotel/api/search/config/ResourceServerConfig.java
@@ -9,10 +9,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class ResourceServerConfig {
 
+    @Value("#{systemEnvironment['HOST-AUTH'] ?: 'localhost'}")
+    private String authServer;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.oauth2ResourceServer(
-                j -> j.jwt().jwkSetUri("http://localhost:9000/auth-server/oauth2/jwks")
+                j -> j.jwt().jwkSetUri("http://"+authServer+":9000/auth-server/oauth2/jwks")
         ).authorizeRequests()
                 .anyRequest().authenticated()
                 .and().build();
